72aefcf2db2d73f242fc408ac7e9a4e14ca9cf1e
Hiroaki Makino <8makino.42tokyo@gmail.com>
2024-11-01 18:53:30 +0900

    done

--- kadai_013/kadai_013.py
+++ kadai_013/kadai_013.py
@@ -1,5 +1,3 @@
-import math
-
 price_tax_ex = 125
 tax = 1.1
 
@@ -8,7 +6,7 @@ def get_price_with_tax(price_tax_ex: int) -> int:
     if price_tax_ex < 0:
         return -1
 
-    price_tax_in = math.floor(price_tax_ex * tax)
+    price_tax_in = price_tax_ex * tax
     return price_tax_in
 
 
